a8caba8fab0762781ec0680c669875805c96eda9
Jyoti Patel <jyotipatel.gweca@gmail.com>
2025-10-26 15:22:59 +0530

    header altered

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,14 +6,14 @@ function Header() {
                 <div>
                     <Link to='/'><img className="h-8 w-8 lg:h-16 lg:w-16 rounded-full" src="jt.jpg" /></Link>
                 </div>
-                <div className="flex md:flex-row gap-2 mt-2 md:gap-6 md:mt-4">
+                <div className="flex flex-row gap-2 mt-2 md:gap-6 md:mt-4">
                     <h1 className="text-xs md:text-2xl font-semibold">Jyoti Patel</h1>
                     <p className='text-xs md:text-sm md:mt-2'>Front-End Developer</p>
                 </div>
                 <div />
                 <div />
                 <div />
-               <div className="flex flex-row items-center justify-center md:justify-end gap-2 md:gap-6 mt-20 md:mt-6">
+               <div className="flex flex-col md:flex-row items-center justify-center md:justify-end gap-2 md:gap-6 mt-20 md:mt-6">
 
                     <Link
                         className="hover:text-pink-500 transition-transform duration-300 hover:scale-110"
