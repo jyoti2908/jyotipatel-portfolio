f11906bc9eb63a0a37daae7a566f4a7069b5a3cf
Jyoti Patel <jyotipatel.gweca@gmail.com>
2025-10-26 19:37:51 +0530

    contact modified

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,14 +7,14 @@ function Footer() {
         <div className="mx-auto flex flex-col md:flex-row bg-cover bg-no-repeat md:p-4 p-2 justify-between h-auto w-full"
          style={{ backgroundImage: "url('footer.webp')"}}>
             <div>
-                <h1 className="text-xs md:text-center md:text-sm text-gray-400">Email </h1>
+                <h1 className="text-xs text-center md:text-sm text-gray-400">Email </h1>
                 <div className="flex justify-center gap-2 md:gap-4 mt-2">
                     <a href='https://mail.google.com/mail/'><TfiEmail className="text-blue-400 md:h-6 md:w-4 md:ml-4 h-4 w-4 mx-auto" /></a>
                     <a className="hover:text-blue-500 md:text-sm text-xs" href='https://mail.google.com/mail/'>jyotipatel.gweca@gmail.com </a>
                 </div>
             </div>
             <div>
-                <h1 className="text-xs md:text-center md:text-sm text-gray-400 mt-4 md:mt-0">Get Connected</h1>
+                <h1 className="text-xs text-center md:text-sm text-gray-400 mt-4 md:mt-0">Get Connected</h1>
                 <div className=" flex flex-col md:gap-2 md:mt-2">
                     <div className="flex md:gap-4 gap-2 justify-center">
                         <div>
@@ -35,7 +35,7 @@ function Footer() {
                 </div>
             </div>
             <div>
-                <p className="mx-auto text-xs md:text-gray-400 text-white mt-4 md:mt-0"> © Designed by Jyoti Patel <br /> All rights reserved.</p>
+                <p className="text-center text-xs md:text-gray-400 text-white mt-4 md:mt-0"> © Designed by Jyoti Patel <br /> All rights reserved.</p>
             </div>
         </div>
     )
