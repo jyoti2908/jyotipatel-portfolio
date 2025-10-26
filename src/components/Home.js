5adeeda247223b04622f2462ca885c3d26d53535
Jyoti Patel <jyotipatel.gweca@gmail.com>
2025-10-26 15:28:55 +0530

    background image responsive

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import Body from "../components/Body";
 
 function Home(){
     return(
-        <div className="bg-cover bg-no-repeat opacity-0 animate-fadeIn h-[700px]"
+        <div className="bg-cover bg-no-repeat opacity-0 animate-fadeIn min-h-[60vh] sm:min-h-[70vh] md:min-h-[90vh] lg:min-h-screen"
                 style={{ backgroundImage: "url('react.jpg')" }}>
           <Body/>
         </div>
